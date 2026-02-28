7d1b862d84d6a3a87186ba7a4f808f09ab9c7ecb
guenox <stephane.guenounoul@gmail.com>
2026-02-28 16:36:35 +0100

    supression du json

--- phase1/demo.py
+++ phase1/demo.py
@@ -1,7 +1,5 @@
 from voronoi import DelaunayTriangulation
 
 mon_interface = DelaunayTriangulation()
-mon_interface.triangulate()
-mon_interface.calculer_centre(0)
-mon_interface.calculer_centre(1)    
+mon_interface.triangulate()   
 mon_interface.afficher_voronoi()
